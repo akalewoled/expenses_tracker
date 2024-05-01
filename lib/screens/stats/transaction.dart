7804d96424b468beb9c55007b6b30b49f7abe256
BaGorK <edmealemkassahun@gmail.com>
2024-05-01 11:05:17 +0300

    feat: create getTiles method | for the bottom text of the graph

--- lib/screens/stats/transaction.dart
+++ lib/screens/stats/transaction.dart
@@ -38,36 +38,81 @@ class Transactionsends StatelessWidget {
     );
   }
 
-
-BarChartData mainBarData() {
-  return BarChartData(
-    titlesData: FlTitlesData(
-      show: true,
-      rightTitles: const AxisTitles(
-        sideTitles: SideTitles(showTitles: false)
-      ),
-      topTitles: const AxisTitles(
-        sideTitles: SideTitles(showTitles: false)
-      ),
-      bottomTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 38,
-          getTitlesWidget: getTiles,
-        )
-      ),
-      leftTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 38,
-          getTitlesWidget: leftTitles,
+  BarChartData mainBarData() {
+    return BarChartData(
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false)
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false)
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 38,
+            getTitlesWidget: getTiles,
+          )
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 38,
+            getTitlesWidget: leftTitles,
+          ),
         ),
       ),
-    ),
-    borderData: FlBorderData(
-      show: false
-    ),
-    gridData: const FlGridData(show: false),
-    barGroups: showingGroups(),
-  );
+      borderData: FlBorderData(
+        show: false
+      ),
+      gridData: const FlGridData(show: false),
+      barGroups: showingGroups(),
+    );
+  }
+
+
+  Widget getTiles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.grey,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    Widget text;
+
+    switch (value.toInt()) {
+      case 0:
+        text = const Text('01', style: style);
+        break;
+      case 1:
+        text = const Text('02', style: style);
+        break;
+      case 2:
+        text = const Text('03', style: style);
+        break;
+      case 3:
+        text = const Text('04', style: style);
+        break;
+      case 4:
+        text = const Text('05', style: style);
+        break;
+      case 5:
+        text = const Text('06', style: style);
+        break;
+      case 6:
+        text = const Text('07', style: style);
+        break;
+      case 7:
+        text = const Text('08', style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16,
+      child: text,
+    );
+  }
 }
